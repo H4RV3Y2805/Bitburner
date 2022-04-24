c1c2c88b7b4b8e35c659ebbeb1290f7048ed0d40
H4RV3Y2805 <lennyharvey85@gmail.com>
2022-04-24 17:27:11 +0100

    converted and deleted buy-servers.js

---
+++ src/starter_scripts/buy-servers.ts
@@ -0,0 +1,111 @@
+import { NS } from "@ns";
+
+export async function main(ns: NS): Promise<void> {
+  const mode = ns.args[0];
+
+  // Check how many servers I already have purchased.
+  // Something
+
+  const purchasedServers = ns.getPurchasedServers();
+  const purchasedServersCount = purchasedServers.length;
+
+  // Check how much money the player has.
+
+  const playerMoney = ns.getServerMoneyAvailable("home");
+  const formattedPlayerMoney = playerMoney.toLocaleString("en-US", {
+    style: "currency",
+    currency: "USD",
+  });
+
+  // Check how many servers I can purchase by checking how much money I have and
+  // dividing it by 25.
+
+  const maxServers = Math.floor(playerMoney / 25);
+  const formattedMaxServers = maxServers.toLocaleString("en-US", {
+    style: "currency",
+    currency: "USD",
+  });
+
+  // Check Mode.
+
+  if (mode == "check") {
+    // Print out how many servers I already have purchased.
+    ns.tprint(
+      "I already have " + purchasedServersCount + " servers purchased."
+    );
+    // Print out how much money I have.
+    ns.tprint("You have: " + formattedPlayerMoney + " available.");
+    // Print max cost of servers.
+    ns.tprint(
+      "I can purchase 25 servers with a max cost of: " + formattedMaxServers
+    );
+    // Print out a line break.
+    ns.tprint(" -------------------------------------------------- ");
+  }
+
+  // Initialize a variable to hold the maximum RAM we can afford.
+  let maxRAM = 0;
+
+  // Check the costs of each server and format the result into US Dollars
+  // Servers start at 2GB RAM and cost $110,000.00 increase by a power of 2 each time with a max power of 20 EG 2^20.
+  for (let i = 1; i <= 20; i++) {
+    // Run the price check for each power of 2.
+    const serverCost = ns.getPurchasedServerCost(Math.pow(2, i));
+    // Format serverCost into US Dollars
+    const formattedServerCost = serverCost.toLocaleString("en-US", {
+      style: "currency",
+      currency: "USD",
+    });
+    // Check if the cost of the server is less than the value of 1/25th of the players money.
+    // If it is, set the maxRAM to the current value of i.
+    if (serverCost < maxServers) {
+      maxRAM = Math.pow(2, i);
+      if (mode == "check") {
+        ns.tprint(
+          "A " +
+            Math.pow(2, i) +
+            "GB server costs: " +
+            formattedServerCost +
+            " and I can purchase 25 of them."
+        );
+      }
+      // If it is not, print out the current value of i and the cost of the server.
+    } else {
+      if (mode == "check") {
+        ns.tprint(
+          "A " + Math.pow(2, i) + "GB server costs: " + formattedServerCost
+        );
+      }
+    }
+  }
+
+  // Print out the max RAM I can purchase.
+  if (mode == "check") {
+    // Print out a line break.
+    ns.tprint(" -------------------------------------------------- ");
+    ns.tprint(maxRAM);
+  }
+
+  // Buy mode.
+
+  const serverPrefix = "pserv-";
+
+  // Using the maxRAM variable, purchase 25 servers.
+  if (mode == "buy") {
+    if (purchasedServersCount == 0) {
+      for (let i = 0; i <= 24; i++) {
+        ns.purchaseServer(serverPrefix + i, maxRAM);
+      }
+    }
+  }
+
+  // Delete mode.
+
+  if (mode == "delete") {
+    // Delete all servers.
+    for (let i = 0; i <= 24; i++) {
+      ns.killall(serverPrefix + i);
+      ns.deleteServer(serverPrefix + i);
+    }
+  }
+}
