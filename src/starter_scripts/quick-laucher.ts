58238b747a1cd3d7e559f72f2918941c48e1e5ad
H4RV3Y2805 <lennyharvey85@gmail.com>
2022-05-06 10:45:07 +0100

    update to quick launcher

--- src/starter_scripts/quick-laucher.ts
+++ src/starter_scripts/quick-laucher.ts
@@ -3,17 +3,17 @@ import { NS } from "@ns";
 export async function main(ns: NS): Promise<void> {
   const scriptArgs = String(ns.args[0]);
 
-  ns.killall();
-
-  const host = ns.getHostname();
-  const threads = threadCheck(host);
+  
 
   function threadCheck(host: string) {
-    const freeRAM = ns.getServerMaxRam(host) - 20 - ns.getServerUsedRam(host);
+    const freeRAM = (ns.getServerMaxRam(host) - 20) - ns.getServerUsedRam(host);
     const scriptRAM = ns.getScriptRam("/starter_scripts/early-hack-template.js", host);
     const scriptThreads = Math.floor(freeRAM / scriptRAM);
     return scriptThreads;
   }
 
+  const host = ns.getHostname();
+  const threads = threadCheck(host);
   ns.exec("/starter_scripts/early-hack-template.js", host, threads, scriptArgs);
+
 }
